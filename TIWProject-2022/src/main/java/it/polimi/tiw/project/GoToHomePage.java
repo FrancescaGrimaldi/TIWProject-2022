825e9bb893070b96f1bc904df32cfd9a7691f7e5
fragr <fragr@DESKTOP-TTPG3QA.homenet.telecomitalia.it>
2022-04-28 17:46:46 +0200

    tried servlets

---
+++ TIWProject-2022/src/main/java/it/polimi/tiw/project/GoToHomePage.java
@@ -0,0 +1,19 @@
+package it.polimi.tiw.project;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/homepage")
+public class GoToHomePage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/plain");
+		PrintWriter out = response.getWriter();
+		out.println("This only tests the GoToHomePage servlet");
+		out.close();
+	}
+}
