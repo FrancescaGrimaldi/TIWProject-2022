46f267d1a0464f11a707cfc37afe115c9522402a
fragr <fragr@DESKTOP-TTPG3QA.homenet.telecomitalia.it>
2022-04-30 00:13:47 +0200

    added User class and login page

---
+++ TIWProject-2022/src/main/java/it/polimi/tiw/project/beans/User.java
@@ -0,0 +1,77 @@
+package it.polimi.tiw.project.beans;
+
+import java.io.Serializable;
+
+/**
+ * This class represents the users of the platform.
+ */
+public class User implements Serializable {
+	private static final long serialversionUID = 1L;
+	
+	private int userID;
+	private String name;
+	private String surname;
+	private String email;
+	private String password;
+	
+	/**
+	 * Class constructor.
+	 */
+	public User(){
+	}
+	
+	public void setID(int id) {
+		this.userID = id;
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public void setSurname(String s) {
+		this.surname = s;
+	}
+	
+	public void setEmail(String e) {
+		this.email = e;
+	}
+	
+	public void setPassword(String p) {
+		this.password = p;
+	}
+	
+	public int getID() {
+		return userID;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getSurname() {
+		return surname;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String toString() {
+		StringBuffer buff = new StringBuffer("User");
+		
+		buff.append(" id: ");
+		buff.append(userID);
+		buff.append(" name: ");
+		buff.append(name);
+		buff.append(" surname: ");
+		buff.append(surname);
+		buff.append(" email: ");
+		buff.append(email);
+		
+		return buff.toString();
+	}
+}
---
+++ TIWProject-2022/src/main/java/it/polimi/tiw/project/controllers/LoginPage.java
@@ -0,0 +1,18 @@
+package it.polimi.tiw.project;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginPage")
+public class LoginPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	
+	
+
+}
